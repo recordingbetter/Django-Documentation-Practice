51e0163d0f5ad878f8c3b0ab329cb7bb378eee0c
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-05 22:06:18 +0900

    homework done

--- djando_app/introduction_to_models/admin.py
+++ djando_app/introduction_to_models/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Person
+from .models.person import Person
+from .models.intermediate import Player, Club, TradeInfo
 
 
 admin.site.register(Person)
+admin.site.register(Player)
+admin.site.register(Club)
+admin.site.register(TradeInfo)
+
--- djando_app/introduction_to_models/models/intermediate.py
+++ djando_app/introduction_to_models/models/intermediate.py
@@ -1,44 +1,110 @@
+from datetime import date
+from django.db.models import Q
+
 from django.db import models
+from django.utils import timezone
 
 
 class Player(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length = 40)
 
     def __str__(self):
         return self.name
-
-    # current_club프로퍼티에 현재 속하는 Club리턴
-    # current_tradeinfo프로퍼티에 현재 자신의 TradeInfo리턴
 
 
 class Club(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length = 40)
     players = models.ManyToManyField(
         Player,
-        through='TradeInfo',
+        through = 'TradeInfo',
+        through_fields = ('club', 'player'),
+        related_name = "%(app_label)s_%(class)s_related",
     )
+
+    # current_club 프로퍼티에 현재 속하는 Club 리턴
+    @property
+    def current_club(self):
+        return 'current club is {}'.format(self.name)
 
     def __str__(self):
         return self.name
 
-    def squad(self, year=None):
-        pass
-        # squad메서드에 현직 선수들만 리턴
+        # squad 메서드에 현직 선수들만 리턴
         # 인수로 년도(2017, 2015...등)를 받아
         # 해당 년도의 현직 선수들을 리턴,
         # 주어지지 않으면 현재를 기준으로 함
+    def squad(self, year = None):
+        if year is None:
+            year = timezone.now().year
+        else:
+            pass
+        player_list = self.players.filter(
+            Q(tradeinfo__date_joined__year__lte = year),
+            (Q(tradeinfo__date_leaved__gte = date(year, 12, 31)) |
+             Q(tradeinfo__date_leaved__isnull = True))
+        )
+        return print(', '.join([p.name for p in player_list]))
 
 
 class TradeInfo(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete = models.CASCADE)
+    club = models.ForeignKey(Club, on_delete = models.CASCADE)
     date_joined = models.DateField()
-    date_leaved = models.DateField(null=True, blank=True)
-    # recommender = models.ForeignKey(Player, on_delete=models.CASCADE)
-    # prev_club = 이전 Club
+    date_leaved = models.DateField(null = True, blank = True)
+    # 2. recommender 와 prev_club(이전 클럽)을 활성화시키고 Club 의 MTM 필드에 through_fields 를 명시
+    recommender = models.ForeignKey(
+        Player,
+        on_delete = models.CASCADE,
+        null = True,
+        blank = True,
+        related_name = 'trade_recommender',
+    )
+    prev_club = models.ForeignKey(
+        Club,
+        on_delete = models.CASCADE,
+        null = True,
+        blank = True,
+        related_name = 'trade_prev_club')
 
-    # 1. property로 is_current 속성이 TradeInfo가 현재 현직(leaved하지 않았는지)여부 반환
-    # 2. recommender와 prev_club을 활성화시키고 Club의 MTM필드에 through_fields를 명시
+    def __str__(self):
+        return 'Player {} is joined club {} at {}.\nAnd left the club at {}'.format(
+            self.player,
+            self.club,
+            self.date_joined,
+            self.date_leaved,
+        )
 
+    # current_tradeinfo 프로퍼티에 현재 자신의 TradeInfo 리턴
+    @property
+    def current_tradeinfo(self):
+        if self.date_leaved:
+            return print('Player {} is joined club {} at {}.\nAnd left the club at {}'.format(
+                self.player,
+                self.club,
+                self.date_joined,
+                self.date_leaved,
+            ))
+        else:
+            return print('Player {} is joined club {} at {}.\nAnd never left.'.format(
+                self.player,
+                self.club,
+                self.date_joined,
+            ))
+            # prev_club = 이전 Club
 
-# 위의 요구조건들을 만족하는 실행코드 작성
+    # 1. property 로 is_current 속성이 TradeInfo 가 현재 현직(leaved 하지 않았는지)여부 반환
+    @property
+    def is_current(self):
+        if self.date_leaved:
+            return print('Player {} is joined club {} at {}.\nAnd left the club at {}'.format(
+                self.player,
+                self.club,
+                self.date_joined,
+                self.date_leaved,
+            ))
+        else:
+            return print('Player {} is joined club {} at {}.\nAnd never left.'.format(
+                self.player,
+                self.club,
+                self.date_joined,
+            ))
